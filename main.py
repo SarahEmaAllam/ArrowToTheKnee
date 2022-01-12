5f7f83d39e7302064ea243dc0c8214c01544115e
Mikko Brandon <m.brandon.1@student.rug.nl>
2022-01-12 20:33:40 +0100

    compact window class

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PySide2.QtWidgets import QApplication, QStackedWidget
 from View.MainWindow import MainWindow
 from Model.ForwardChaining import *
 from Model.Patient import Patient
-from Question_Windows import *
+# from Question_Windows import *
+from View.WindowHandler import *
 import qdarkstyle
 import random
 import time
@@ -26,36 +27,37 @@ if __name__ == "__main__":
     #Stylesheets will give a template for different widgets
     app.setStyleSheet(qdarkstyle.load_stylesheet())
 
-    Main = WindowHandler()
+    # Main = WindowHandler()
+    #
+    # #Styling placeholder class
+    # intro_Window = IntroductionWindow(Main)
+    # age_Window = AgeWindow(Main)
+    # gender_Window = GenderWindow(Main)
+    # pain_start_Window = PainStartWindow(Main)
+    # pain_location_Window = PainLocationWindow(Main)
+    # swelling_location_Winow = SwellingLocationWindow(Main)
+    # swelling_Window = SwellingWindow(Main)
+    # bending_Window = BendingWindow(Main)
+    # activity_Window = ActivitiesWindow(Main)
+    # extension_Window = ExtensionWindow(Main)
+    #
+    # stack = QStackedWidget()
+    # stack.addWidget(intro_Window)
+    # stack.addWidget(age_Window)
+    # stack.addWidget(gender_Window)
+    # stack.addWidget(pain_start_Window)
+    # stack.addWidget(pain_location_Window)
+    # stack.addWidget(swelling_location_Winow)
+    # stack.addWidget(swelling_Window)
+    # stack.addWidget(bending_Window)
+    # stack.addWidget(activity_Window)
+    # stack.addWidget(extension_Window)
+    #
+    # Main.addStack(stack)
+    #
+    # Main.displayWindow()
 
-    #Styling placeholder class
-    intro_Window = IntroductionWindow(Main)
-    age_Window = AgeWindow(Main)
-    gender_Window = GenderWindow(Main)
-    pain_start_Window = PainStartWindow(Main)
-    pain_location_Window = PainLocationWindow(Main)
-    swelling_location_Winow = SwellingLocationWindow(Main)
-    swelling_Window = SwellingWindow(Main)
-    bending_Window = BendingWindow(Main)
-    activity_Window = ActivitiesWindow(Main)
-    extension_Window = ExtensionWindow(Main)
-
-    stack = QStackedWidget()
-    stack.addWidget(intro_Window)
-    stack.addWidget(age_Window)
-    stack.addWidget(gender_Window)
-    stack.addWidget(pain_start_Window)
-    stack.addWidget(pain_location_Window)
-    stack.addWidget(swelling_location_Winow)
-    stack.addWidget(swelling_Window)
-    stack.addWidget(bending_Window)
-    stack.addWidget(activity_Window)
-    stack.addWidget(extension_Window)
-
-    Main.addStack(stack)
-
-    Main.displayWindow()
-
+    main_window = WindowHandler()
 
     #var = True
     #print(dict)
