f52fd30198dc82689401c53787545ab3fe3ef471
Mikko Brandon <m.brandon.1@student.rug.nl>
2022-01-19 15:39:21 +0100

    no error when sliders are 0

--- View/Window.py
+++ View/Window.py
@@ -5,23 +5,25 @@ from PySide2.QtWidgets import QMainWindow, QVBoxLayout, QLabel, QWidget, QSizePo
 from PySide2.QtCore import QSize
 from PySide2.QtGui import QFont
 
-# current_diagnosis = 'None'
-
 
 class Window(QMainWindow):
-    def __init__(self, main_window, question, widgets, qtype="normal"):
+    def __init__(self, main_window, question, widgets, qtype):
         super().__init__()
+        # Standard window settings
         self.main_window = main_window
         self.setMinimumSize(QSize(1000, 600))
         self.layout = QVBoxLayout()
+
         self.question = question
         self.qtype = qtype
         self.widgets = []
         self.symptoms = []
 
+        # Construct window
         self.set_widgets(widgets)
         self.construct_window()
 
+    # Function for constructing the elements of a window
     def set_widgets(self, widgets):
         # Standard window buttons
         if self.qtype == "normal":
@@ -34,6 +36,7 @@ class Window(QMainWindow):
 
         # Multi choice: text labels with sliders
         elif self.qtype == "multi":
+
             for slider in widgets:
                 # Create text label for movement type
                 new_label = QLabel(slider[0])
@@ -50,34 +53,34 @@ class Window(QMainWindow):
             new_button.clicked.connect(self.was_clicked)
             self.widgets.append(new_button)
 
-        # diagnosis = QLabel()
-        # diagnosis.setText("Temporary Diagnosis: " + current_diagnosis)
-        # self.widgets.append(diagnosis)
-
         # Show temporary diagnosis if there is one
         if self.main_window.diagnosis:
             diagnosis = QLabel()
             diagnosis.setText("Temporary Diagnosis: " + self.main_window.diagnosis)
             self.widgets.append(diagnosis)
 
+    # Function defining button behavior when clicked, calls forward chaining process
     def was_clicked(self):
-        if self.sender().text() != "Next":
+
+        if self.sender().text() == "Next":
+            if not self.symptoms:
+                # Statement to ensure no error is met when sliders are not adjusted
+                self.symptoms.append(["still", 0])
+        else:
             self.symptoms.append(self.sender().symptom)
 
-        stack = self.main_window.stack
+        # Skip follow-up question whenever it is irrelevant
         if self.sender().text() == "No":
             if self.question == "Does bending or extending the knee hurt?" or self.question == "Is there swelling of " \
                                                                                                "the knee?":
                 self.main_window.idx += 1
 
-        print("current ques", self.question)
-        print("HEYEYEYEYEY ", self.symptoms)
-        # forward_chaining(self, self.main_window.patient)
-        result = forward_chaining(self, self.main_window.kb, self.symptoms)
-        print('current diaf', result)
-        self.main_window.diagnosis = result
+        # Store result of forward chaining with obtained facts as temporary diagnosis
+        temp_diagnosis = forward_chaining(self, self.main_window.kb, self.symptoms)
+        self.main_window.diagnosis = temp_diagnosis
         self.main_window.show_next_screen()
 
+    # Function defining slider behavior
     def value_changed(self):
         # Iterates through the already found values for sliders
         for symptom in self.symptoms:
@@ -92,6 +95,7 @@ class Window(QMainWindow):
         # Add new nonzero symptom value to list
         self.symptoms.append([self.sender().symptom, str(self.sender().value())])
 
+    # Function that constructs the window
     def construct_window(self):
         text_label = QLabel(self.question)
         text_label.setAlignment(Qt.AlignCenter)
@@ -99,6 +103,7 @@ class Window(QMainWindow):
         font = QFont("Cambria", 25, QFont.Bold)
         text_label.setFont(font)
         self.layout.addWidget(text_label)
+
         for widget in self.widgets:
             self.layout.addWidget(widget)
 
